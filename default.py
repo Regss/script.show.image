ecf2d1c88a67101dcc4283523fd775cc700076b7
Piotr Radziewicz <regss84@gmail.com>
2015-08-26 22:26:43 +0200

    1.1.0

--- default.py
+++ default.py
@@ -3,6 +3,7 @@
 import xbmcgui
 import xbmc
 import xbmcaddon
+import json
 from PIL import Image
 
 __addon__           = xbmcaddon.Addon()
@@ -13,14 +14,23 @@ __addonpath__       = xbmc.translatePath(__addon__.getAddonInfo('path')).decode(
 __lang__            = __addon__.getLocalizedString
 __path_img__        = __addonpath__ + "/images"
 
-ACTION_PREVIOUS_MENU = 10
-ACTION_MOVE_LEFT = 1
-ACTION_MOVE_RIGHT = 2
-ACTION_MOVE_UP = 3
-ACTION_MOVE_DOWN = 4
+class ShowImage:
+    def __init__(self):
+        window = xbmcgui.Window(xbmcgui.getCurrentWindowId())
+        if window.getProperty('ShowImage') != 'true':
+            display = windowImage()
+            display .doModal()
+            del display
+            window.setProperty('ShowImage', 'false')
+        else:
+            xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Input.ExecuteAction", "params":["back"]},"id":1}')
 
 class windowImage(xbmcgui.WindowDialog):
     def __init__(self):
+    
+        window = xbmcgui.Window(xbmcgui.getCurrentWindowId())
+        window.setProperty('ShowImage', 'true')
+        
         # check arg getting from settings
         try:
             idImage = str(sys.argv[1])
@@ -30,10 +40,10 @@ class windowImage(xbmcgui.WindowDialog):
         imagePath = __addon__.getSetting('image'+idImage)
         
         # set max height and margin to background
-        xImageRes = 600
+        xImageRes = 660
         yImageRes = 400
-        yMax = 600
-        margin = 20
+        yMax = 660
+        margin = 10
         
         if idImage == '0':
             msg = __lang__(32100)    
@@ -64,10 +74,6 @@ class windowImage(xbmcgui.WindowDialog):
         yBgRes = yImageRes + (margin * 4)
         xBgPos = xImagePos - margin
         yBgPos = yImagePos - margin
-        xButtonRes = 100
-        yButtonRes = 30
-        xButtonPos = ((1280 - xButtonRes) / 2)
-        yButtonPos = yImagePos + yImageRes + (margin / 2) 
         
         # create window
         self.bg = xbmcgui.ControlImage(xBgPos,yBgPos,xBgRes,yBgRes, __path_img__ + '//bg.png')
@@ -78,19 +84,12 @@ class windowImage(xbmcgui.WindowDialog):
         else:
             self.label = xbmcgui.ControlLabel(xImagePos,yImagePos,xImageRes,yImageRes, msg, 'font13', alignment=6)
             self.addControl(self.label)
-        self.button = xbmcgui.ControlButton(xButtonPos, yButtonPos, xButtonRes, yButtonRes, "OK", alignment=6, font='font12')
-        self.addControl(self.button)
-        self.setFocus(self.button)
 
     def onAction(self, action):
-        if action == ACTION_PREVIOUS_MENU:
-            self.close()
-        if action == ACTION_MOVE_LEFT or action == ACTION_MOVE_RIGHT or action == ACTION_MOVE_UP or action == ACTION_MOVE_DOWN:
-            self.setFocus(self.button)
+        self.close()
+        
     def onControl(self, control):
-        if control == self.button:
-            self.close()
-            
-display = windowImage()
-display .doModal()
-del display
+        self.close()
+
+ShowImage()
+
